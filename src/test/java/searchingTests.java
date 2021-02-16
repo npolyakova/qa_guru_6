c098601fe43c4deec72f99172dafc2ca255c3929
Natalya <n.polyakova.kazanexpress@gmail.com>
2021-02-16 09:18:49 +0300

    added gradle, gitignore, test

---
+++ src/test/java/searchingTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class searchingTests {
+
+    @Test
+    void selenideSearchTest() {
+        open("https://www.google.com/");
+        $(byName("q")).setValue("Selenide").pressEnter();
+        $("#search").shouldHave(text("selenide.org"));
+    }
+}
